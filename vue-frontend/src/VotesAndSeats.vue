d933cd5289d012f6012acf7691ce294cc32b293d
Kristján Jónasson <jonasson@hi.is>
2021-07-29 08:50:04 +0000

    Vinna við að splitta calculator

--- vue-frontend/src/VotesAndSeats.vue
+++ vue-frontend/src/VotesAndSeats.vue
@@ -5,31 +5,6 @@
       @update-vote-table="updateVoteTable"
       @server-error="serverError">
     </VoteMatrix>
-
-    <b-alert :show="server.waitingForData">Loading...</b-alert>
-    <b-alert :show="server.error" dismissible @dismissed="server.error=false" variant="danger">Server error. Try again in a few seconds...</b-alert>
-    <b-alert :show="server.errormsg != ''" dismissible @dismissed="server.errormsg=''" variant="danger">Server error. {{server.errormsg}}</b-alert>
-
-    <b-tabs style="margin-top:10px">
-      <b-tab title="Single Election" active>
-        <Election
-          :server="server"
-          :vote_table="vote_table"
-          :election_rules="election_rules"
-          :activeTabIndex="activeTabIndex"
-          @update-rules="updateElectionRules">
-        </Election>
-      </b-tab>
-      <b-tab title="Simulation">
-        <Simulate
-          :server="server"
-          :vote_table="vote_table"
-          :election_rules="election_rules"
-          :simulation_rules="simulation_rules"
-          @update-rules="updateSimulationRules">
-        </Simulate>
-      </b-tab>
-    </b-tabs>
   </div>
 </template>
 
@@ -37,12 +12,11 @@
 import Election from './Election.vue'
 import Simulate from './Simulate.vue'
 import VoteMatrix from './components/VoteMatrix.vue'
-import ElectionSettings from './components/ElectionSettings.vue'
+import import ElectionSettings from './components/ElectionSettings.vue'
 
 export default {
   components: {
     VoteMatrix,
-    ElectionSettings,
     Election,
     Simulate,
   },
