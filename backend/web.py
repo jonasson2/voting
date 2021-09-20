47483fa50a39c4c2cc6d8606f7beee987efa3c5e
Kristján Jónasson <jonasson@hi.is>
2021-09-20 10:57:56 +0000

    fix frá Pétri

--- backend/web.py
+++ backend/web.py
@@ -44,8 +44,8 @@ CORS(app)
 @app.route('/')
 def serve_index():
     digoce = os.environ.get("FLASK_DIGITAL_OCEAN", "") == "True"
-    # indexfile = "index-digital-ocean.html" if digoce else "index.html"
-    indexfile = "index.html"
+    indexfile = "index-digital-ocean.html" if digoce else "index.html"
+    #indexfile = "index.html"
     print("Calling serve_index with indexfile", indexfile)
     return render_template(indexfile)
 
