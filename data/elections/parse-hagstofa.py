dc4b10fc297ba898a6a11dceadb95ed5f18791dc
Kristján Jónasson <jonasson@hi.is>
2021-09-26 10:21:09 +0000

    Adding 1987-1999

---
+++ data/elections/parse-hagstofa.py
@@ -0,0 +1,47 @@
+import csv
+flo = {}
+kjd = {}
+ár = {}
+tafla = {}
+ár['25. apríl 1987'] = "1987"
+ár['20.apr.91'] = "1991"
+ár['8. apríl 1995'] = "1995"
+ár['8. maí 1999'] = "1999"
+
+with open("kjordaemi.csv") as kj:
+    for (skst,kjörd) in csv.reader(kj):
+        kjd[kjörd] = skst
+nkjördæmi = len(kjd)
+
+tafla = {}
+flokkar = {}
+
+for y in ár.values():
+    tafla[y] = [[] for i in range(nkjördæmi)]
+    flokkar[y] = []
+
+with open("flokkar.csv") as fl:
+    for (skst,flokkur) in csv.reader(fl, delimiter="\t"):
+        flo[flokkur] = skst
+
+with open("hagstofa-87-99.csv") as ha:
+    reader = csv.reader(ha, quoting=csv.QUOTE_NONNUMERIC, delimiter=";")
+    kjördæmi = next(reader)[2:]
+    for L in reader:
+        dags = L[0]
+        y = ár[dags]
+        fl = flo[L[1]]
+        flokkar[y].append(fl)
+        atkv = L[2:]
+        if not sum(atkv): continue
+        for k in range(nkjördæmi):
+            tafla[y][k].append(atkv[k])
+
+print(kjd.values())
+            
+for y in ár.values():
+    for k in range(nkjördæmi):
+        kd = list(kjd.values())[k]
+        print(f'{y}  {kd:15}  ',end='')
+        for a in tafla[y][k]: print(f'{a:6.0f} ', end='')
+        print()
