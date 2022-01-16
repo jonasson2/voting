7b77d566fedcba86191c3ac30ef209737e6f2e57
Kristján Jónasson <jonasson@hi.is>
2022-01-16 15:29:39 +0000

    version 2.12

--- vue-frontend/src/Intro.vue
+++ vue-frontend/src/Intro.vue
@@ -4,7 +4,7 @@
 
 
   <p>
-    Election Simulator, version: 2.11(beta) Time of release: 2022-01-10
+    Election Simulator, version: 2.12(beta) Time of release: 2022-01-16
   </p>
   <p>
     The aim of this software is to help people understand
