fcaad3861578e5774b231da5a4f704f49e73908e
Kristján Jónasson <jonasson@hi.is>
2022-07-01 11:44:53 +0000

    xx

--- bin/runvoting.sh
+++ bin/runvoting.sh
@@ -3,4 +3,5 @@ pwd=$(pwd)
 dir=${pwd#$HOME/}
 rootdir=$HOME/${dir%%/*}
 echo Running in $rootdir
+echo "(pwd=$pwd, dir=$dir)"
 (cd $rootdir/vue-frontend; npm run build) && (cd $rootdir/backend; python web.py)
