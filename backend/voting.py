a5cc1519e8650940bb42d59b00f32825101834d7
Lilja Jonsdottir <liljasteinunn@gmail.com>
2023-02-06 12:39:24 +0100

    tviskolun lagað nákvæmni

--- backend/voting.py
+++ backend/voting.py
@@ -328,7 +328,8 @@ class Election:
         if ncols > 1 and nrows > 1:
             iter = 0
             if row_constraints and col_constraints:
-                if sum(col_sums) == sum(row_sums): # förum í einfaldari gerð ef það eru jöfnur
+                '''
+                if sum(col_sums) != sum(row_sums): # förum í einfaldari gerð ef það eru jöfnur TODO: laga
                     error = 1
                     while round(error, 7) != 0.0:
                         error = 0
@@ -345,49 +346,49 @@ class Election:
                             ref_seat_shares[:, p] *= tau
                             error = max(error, abs(1 - tau))
                 else:
-                    while True:
-                        iter+=1
-                        # constituency step
-                        for c in range(nrows):
-                            s = sum(ref_seat_shares[c, :])
-                            eta = s/row_sums[c] if row_sums[c] > 0 else 0
-                            if eta == 0:
-                                ref_seat_shares[c, :] *= 0
+                '''
+                while True:
+                    iter+=1
+                    # constituency step
+                    for c in range(nrows):
+                        s = sum(ref_seat_shares[c, :])
+                        eta = s/row_sums[c] if row_sums[c] > 0 else 0
+                        if eta == 0:
+                            ref_seat_shares[c, :] *= 0
+                        else:
+                            ref_seat_shares[c, :] /= eta
+                    # party step
+                    iii = 0
+                    over = list(filter(lambda p: round(sum(ref_seat_shares[:, p]), 9) > col_sums[p], range(ncols)))
+                    while over:
+                        iii +=1
+                        H = list(filter(lambda p:
+                                        round(sum(ref_seat_shares[:, p]), 9) >= col_sums[p],
+                                        range(ncols)))
+                        not_H = list(set(range(ncols)) - set(H))
+                        for p in H:
+                            s = sum(ref_seat_shares[:, p])
+                            tau = s / col_sums[p] if col_sums[p] > 0 else 0
+                            if tau == 0:
+                                ref_seat_shares[:, p] *= 0
                             else:
-                                ref_seat_shares[c, :] /= eta
-                        # party step
-                        iii = 0
-                        while list(filter(lambda p: sum(ref_seat_shares[:, p]) > col_sums[p], range(ncols))):
-                            iii +=1
-                            H = list(filter(lambda p:
-                                            sum(ref_seat_shares[:, p]) >= col_sums[p],
-                                            range(ncols)))
-                            not_H = list(set(range(ncols)) - set(H))
-                            for p in H:
-                                s = sum(ref_seat_shares[:, p])
-                                tau = s / col_sums[p] if col_sums[p] > 0 else 0
+                                ref_seat_shares[:, p] /= tau
+                        if not_H:
+                            s = sum(sum(ref_seat_shares[:, p]) for p in not_H)
+                            tau = s/(self.total_const_seats-sum(col_sums[p] for p in H))
+                            for p in not_H:
                                 if tau == 0:
                                     ref_seat_shares[:, p] *= 0
                                 else:
                                     ref_seat_shares[:, p] /= tau
-                            if not_H:
-                                s = sum(sum(ref_seat_shares[:, p]) for p in not_H)
-                                tau = s/(self.total_const_seats-sum(col_sums[p] for p in H))
-                                for p in not_H:
-                                    if tau == 0:
-                                        ref_seat_shares[:, p] *= 0
-                                    else:
-                                        ref_seat_shares[:, p] /= tau
-                            if iii > 100:
-                                print('Calculate_ref_seat_shares: endless party step encountered')
-                                break
-                    # exit condition
-                        if m.isclose(sum(abs(sum(ref_seat_shares[c, :])-row_sums[c])
-                                         for c in range(nrows)), 0.0, abs_tol=1e-7) and \
-                            m.isclose(sum(max(sum(ref_seat_shares[:, p])-col_sums[p], 0.0)
-                                          for p in range(ncols)), 0.0, abs_tol=1e-7):
-                            break
-                    print(f'calculate_ref_seat_shares:\titeration count = {iter}')
+                        over = list(filter(lambda p: round(sum(ref_seat_shares[:, p]), 7) > col_sums[p], range(ncols)))
+                # exit condition
+                    if m.isclose(sum(abs(sum(ref_seat_shares[c, :])-row_sums[c])
+                                     for c in range(nrows)), 0.0, abs_tol=1e-7) and \
+                        m.isclose(sum(max(sum(ref_seat_shares[:, p])-col_sums[p], 0.0)
+                                      for p in range(ncols)), 0.0, abs_tol=1e-7):
+                        break
+                print(f'calculate_ref_seat_shares:\titeration count = {iter}')
             elif row_constraints:
                 for c in range(nrows):
                     row_sum = row_sums[c]
